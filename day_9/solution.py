5b5b06bb0e47ff3677d55b9d9b989b8c6fefff5d
Tristan Collen <tcollen@hotmail.co.uk>
2023-12-09 20:06:14 +0000

    day 9 part 2

--- day_9/solution.py
+++ day_9/solution.py
@@ -5,20 +5,27 @@ def get_diffs(nums):
     return [nums[i + 1] - nums[i] for i in range(len(nums) - 1)]
 
 
-def traverse(nums):
+def traverse(nums, handler):
     diffs = get_diffs(nums)
 
     if all(v == 0 for v in nums):
         return 0
     else:
-        new_diffs = traverse(diffs)
-        return nums[-1] + new_diffs
+        new_diffs = traverse(diffs, handler)
+        return handler(nums, new_diffs)
 
 
 def solution_one(problem_input):
+    def handler(nums, diffs):
+        return nums[-1] + diffs
+
     parsed = text_to_int_list(problem_input)
-    return sum([traverse(num_arr) for num_arr in parsed])
+    return sum([traverse(num_arr, handler) for num_arr in parsed])
 
 
-def solution_two(input):
-    return sum([traverse(num_arr) for num_arr in input])
+def solution_two(problem_input):
+    def handler(nums, diffs):
+        return nums[0] - diffs
+
+    parsed = text_to_int_list(problem_input)
+    return sum([traverse(num_arr, handler) for num_arr in parsed])
